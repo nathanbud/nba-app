18e339958ce464fc27983752327749c7212d646e
nathanbud <nthnlbdky916@gmail.com>
2019-06-22 17:58:00 +0800

    redo all task

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
-import React, {Component}from 'react';
-import {Route, Switch} from 'react-router-dom'
-import Home from './Component/Home/Home'
-import Layout from './hoc/Layout/layout'
+import React, {Component} from 'react';
+import { Route, Switch} from 'react-router-dom'
+
+import Home from './components/Home/home'
+
+
 
 class Routes extends Component{
     render(){
         return(
-            <Layout>
-                <Switch>
-                    <Route path = '/' component = {Home}/>
-                </Switch>
-            </Layout>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+            </Switch>
         )
     }
-
 }
 
 export default Routes
