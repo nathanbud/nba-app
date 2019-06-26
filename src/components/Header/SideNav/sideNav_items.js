370c757edfbe3bf3da7e19eac8fef2f0032c2331
nathanbud <nthnlbdky916@gmail.com>
2019-06-26 23:13:22 +0800

    Modified and Added NAv Links

--- src/components/Header/SideNav/sideNav_items.js
+++ src/components/Header/SideNav/sideNav_items.js
@@ -4,15 +4,41 @@ import FontAwesome from 'react-fontawesome';
 import style from './sideNav.module.css'
 
 const SideNavItems = () =>{
-    return(
-        <div className = {style.option}>
-            <Link to="/">
-                <FontAwesome name="home"/>
-                Home
-            </Link>
-        </div>
 
-    )
-}
+    const items = [
+        {
+            type: style.option,
+            icon: 'home',
+            text: 'Home',
+            link: '/'
+        },
+        {
+            type: style.option,
+            icon: 'file-text-o',
+            text: 'News',
+            link: '/news'
+        },
+    ]
+    const showItems =()=>{
+        return items.map((item, index)=>{
+                return(
+                    <div key = {index} className = {item.type}>
+                        <Link to = {item.link}>
+                            <FontAwesome name = {item.icon}/>
+                            {item.text}
+                        </Link>
+                    </div>
+            
+                )
+            })
+        }
+        return(
+            <div>
+            {showItems()}
+                </div>
+            )
+    }
+
+  
 
 export default SideNavItems;
