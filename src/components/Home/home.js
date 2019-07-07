04f07eb8c74febee61508e2a7082cc62f4c46399
nathanbud <nthnlbdky916@gmail.com>
2019-07-08 00:39:43 +0800

    Created VideosList component

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NewsSlider from '../widgets/NewsSlider/slider';
 import NewsList from '../widgets/NewsList/newsList';
+import VideosList from '../widgets/VideosList/videosList';
 
 
 const Home = () =>{
@@ -14,6 +15,14 @@ const Home = () =>{
                 loadmore={true}
                 start={3}
                 amount={3} 
+                />
+
+                <VideosList
+                    type="card"
+                    title = {true}
+                    loadmore = {false}
+                    start = {0}
+                    amount = {3}
                 />
         </div>
     )
---
+++ src/components/widgets/VideosList/videosList.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react'
+import styles from './videosList.module.css'
+import axios from 'axios';
+
+import { URL } from '../../../helper';
+import Button from '../Buttons/buttons'
+
+class VideosList extends Component{
+
+    render(){
+        return(
+        <div>
+            VideosList
+        </div>
+        )
+        
+    }
+}
+
+export default VideosList;
