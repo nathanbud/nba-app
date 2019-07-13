bda4d72f516e584a8cf78c2bb06dfc7833e07215
nathanbud <nthnlbdky916@gmail.com>
2019-07-13 16:57:37 +0800

    created Header and Body Components

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { Route, Switch} from 'react-router-dom'
 import Home from './components/Home/home'
 import Layout from './hoc/Layout/layout'
 
+import NewsArticle from './components/Articles/News/Post/index'
+
 
 
 class Routes extends Component{
@@ -12,6 +14,7 @@ class Routes extends Component{
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
+                    <Route path="/articles/:id" exact component={NewsArticle}/>
                 </Switch>
             </Layout>
         )
