9bc6f2ddfb9bba9985cd4e0b112f57f81c6165e5
nathanbud <nthnlbdky916@gmail.com>
2019-07-31 23:34:34 +0800

    Completed VideoList

--- src/components/Articles/Videos/Main/index.js
+++ src/components/Articles/Videos/Main/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import VideosList from '../../../widgets/VideosList/videosList';
 
 const VideosMain = () =>(
-<div>
-        videos main
-    </div>
+   <VideosList
+    type="card"
+    title= {false}
+    loadmore={true}
+    start={0}
+    amount={10}
+   />
 
    )
 
